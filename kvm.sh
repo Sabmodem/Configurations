d3c693d515993fd26771b2d0795e95027d2523f7
Igor Zmitrachkov <zmitrachkov_igor@mail.ru>
2021-06-08 19:09:26 +0000

    added netcard, serial port

--- kvm.sh
+++ kvm.sh
@@ -1,18 +1,22 @@
-#!/bin/zsh
+#!/usr/bin/zsh
 
 QEMU="/usr/bin/qemu-system-x86_64"
 
 ${QEMU} \
     -enable-kvm \
     -m 4096 \
-    -machine q35,accel=kvm \
-    -cpu host,hv_relaxed,hv_vapic,hv_spinlocks=0x1000 \
-    -smp 4,sockets=1,cores=4,threads=1 \
-    -drive if=pflash,format=raw,readonly,file=/usr/share/ovmf/x64/OVMF.fd \
-    -drive if=pflash,format=raw,file=/usr/share/ovmf/x64/OVMF_VARS.fd \
+    -drive if=pflash,format=raw,unit=0,file=/usr/share/OVMF/OVMF_CODE.fd,readonly=on \
+    -drive if=pflash,format=raw,unit=1,file=/usr/share/OVMF/OVMF_VARS.fd \
     -vga std \
     -device usb-ehci,id=ehci -device usb-host,bus=ehci.0,vendorid=0x0951,productid=0x1666 \
-    -boot d \
-    -cdrom /home/sabmodem/brokenArch.iso \
-    # -cdrom /home/sabmodem/install-amd64-minimal-20201118T214503Z.iso \
-    # -hda gentoo.qcow2 \
+    -device usb-ehci,id=ehci1 -device usb-host,bus=ehci.0,vendorid=0x0951,productid=0x1642 \
+    -cdrom  /home/sabmodem/Downloads/archlinux-2020.06.01-x86_64.iso \
+    -net nic,model=virtio \
+    -net user,hostfwd=tcp::2222-:22 \
+    -serial /dev/ttyS0
+    # -net user,hostfwd=tcp::10022-:22
+    # -net nic
+    # -hda /home/sabmodem/qemu_imgs/win_serv.qcow2
+    # -machine q35,accel=kvm \
+    # -cpu host,hv_relaxed,hv_vapic,hv_spinlocks=0x1000 \
+    # -smp 4,sockets=1,cores=4,threads=1 \
